e46df8ca3c62d426ba6ff9adbae4235c6783393e
Lisandra Lopes Roque <lisandra.roque@digisystem.com.br>
2020-03-16 13:18:44 -0300

    Organizando o repositório em pastas

---
+++ Exercícios/lista_03/src/main/java/com/mycompany/lista_03/Acumulador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lista_03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lisandra.roque
+ */
+//Criar uma classe chamada “Acumulador” que :
+//Leia vários números até ler o número 0 e calcule a soma dos números lidos. 
+//(Não é para usar vetor)
+public class Acumulador {
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        
+        
+    }
+}
---
+++ Exercícios/lista_03/src/main/java/com/mycompany/lista_03/Sorteio.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lista_03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lisandra.roque
+ */
+//Crie uma classe chamada “Sorteio” que:
+//Solicite um número de 1 a 100. Realize 200 sorteios. Ao final, exibir:
+//Se o número do usuário foi sorteado (pela 1ª vez) e em qual sorteio. 
+//Quantos números pares foram sorteados. Quantos números ímpares foram sorteados.
+public class Sorteio {
+    public static void main(String[] args) {
+        Integer numero;
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite um número de 1 a 100:");
+        numero = leitor.nextInt();
+        
+        
+   
+    }
+}
